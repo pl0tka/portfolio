384333964e9b08a3543ea2292ad0c8ff158cfc0c
plotva <huseva.a@gmail.com>
2024-07-21 17:57:51 +0200

    Refactor Project component: list technologies and remove unused classes

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,12 @@ import React from 'react';
 function Project({ project }) {
   const { title, desc, techlonogies, img, projectLink, githubLink } = project;
 
+  const renderedTechnologies = techlonogies.map((item, index) => (
+    <span className="project-technology" key={index}>
+      {item}
+    </span>
+  ));
+
   return (
     <article className="project">
       <div className="project-image-wrapper">
@@ -10,16 +16,19 @@ function Project({ project }) {
       </div>
       <div className="project-info-wrapper">
         <h3 className="project-title">{title}</h3>
-        <p>{desc}</p>
-        <p>Technologies used: {techlonogies}</p>
+        <p className="project-description">{desc}</p>
+        <p className="project-tech-stack">
+          Technologies:
+          {renderedTechnologies}
+        </p>
       </div>
       <div className="project-btns-wrapper">
-        <button className="btn project-btn">
+        <button className="project-btn">
           <a href={projectLink} target="_blank">
             visit site
           </a>
         </button>
-        <button className="btn project-btn">
+        <button className="project-btn">
           <a href={githubLink} target="_blank">
             github repo
           </a>
