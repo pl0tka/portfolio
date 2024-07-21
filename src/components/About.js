975375531321204d1eb8b01b184163d5d9effe24
plotva <huseva.a@gmail.com>
2024-07-21 17:58:56 +0200

    Refactor About component: list technologies and update description

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import technologies from '../data/aboutTechnologies';
 
 function About() {
+  const lastItem = technologies.length - 1;
+  const renderedTechnologies = technologies.map((item, index) =>
+    index === lastItem ? (
+      <span key={index}>{item}</span>
+    ) : (
+      <span key={index}>{item} | </span>
+    )
+  );
+
   return (
     <section id="about" className="section">
       <h2 className="section-title">about me</h2>
       <div className="about">
         <p>
           My coding journey began two years ago during an internship in a Fraud
-          team where I picked up SQL. That experience hooked me on programming,
-          and I've been learning it ever since. I’m a curious, friendly, and
-          motivated person who loves creating beautiful things and exploring new
-          technologies. Now, I’m looking forward to my next adventure as a
-          full-stack developer.
+          team where I picked up SQL. That experience hooked me on programming!
+          😍 I started with front-end technologies, then moved on to back-end
+          development to become a well-rounded full-stack developer. I’m a
+          curious, friendly, and motivated person who loves creating beautiful
+          things and exploring new technologies. Now, I’m looking forward to my
+          next adventure as a full-stack developer.
         </p>
         <p className="about-tech-stack">
           <span className="about-accent">My tech stack: </span>
-          JavaScript | React | Node.js | Express | MongoDB | HTML5 | CSS3 | Sass
-          | styled-components | Tailwind | Bulma
+          {renderedTechnologies}
         </p>
       </div>
     </section>
---
+++ src/data/aboutTechnologies.js
@@ -0,0 +1,15 @@
+const technologies = [
+  'JavaScript',
+  'React',
+  'Node.js',
+  'Express',
+  'MongoDB',
+  'HTML5',
+  'CSS3',
+  'Sass',
+  'styled-components',
+  'Tailwind',
+  'Bulma',
+];
+
+export default technologies;
