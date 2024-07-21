b7c51e35a8f957e75857d5f3141ed220aa76b1bc
plotva <huseva.a@gmail.com>
2024-07-21 18:19:09 +0200

    Add test projects sample and project image

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+import screenshot1 from '../img/screen-1.png';
+import screenshot2 from '../img/screen-2.png';
+import screenshot3 from '../img/screen-3.png';
+
+const projects = [
+  {
+    title: 'Online Store Project',
+    desc: 'This is an online store project featuring product display, cart management, detailed product information viewing, and a responsive layout.',
+    techlonogies: ['JavaScript', 'HTML', 'CSS', 'Sass'],
+    img: screenshot3,
+    projectLink: 'https://only-best-prices.netlify.app/',
+    githubLink: 'https://github.com/pl0tka/eStore',
+  },
+  {
+    title: 'Memory Cards Game',
+    desc: 'Test your memory skills by clicking on cards to earn points. After each click, the cards are shuffled randomly. Be careful — clicking the same card twice ends the game!',
+    techlonogies: ['HTML', 'CSS', 'Sass', 'JavaScript'],
+    img: screenshot2,
+    projectLink: '#',
+    githubLink: '#',
+  },
+  {
+    title: 'To-do App',
+    desc: 'A simple to-do app using the MERN stack (MongoDB, Express, React, Node.js). It features task management with CRUD operations, real-time updates, and a clean interface for easy use.',
+    techlonogies: [
+      'JavaScript',
+      'React',
+      'Node.js',
+      'MongoDB',
+      'HTML',
+      'CSS',
+      'MongoDB',
+      'HTML',
+      'CSS',
+    ],
+    img: screenshot1,
+    projectLink: '#',
+    githubLink: '#',
+  },
+];
+
+export default projects;
