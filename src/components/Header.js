db90f2f4203fa22cde2f8f6cd21206fb3341a4c7
plotva <huseva.a@gmail.com>
2024-07-22 18:26:34 +0200

    Refactor Nav and NavToggler interations:
    - Close nav on anchor link click and add smooth scrolling with offset in Nav
    - Remove focus from NavToggler on click
    - Simplify Nav by removing media query logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import Nav from './Nav';
 import NavToggler from './NavToggler';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Header() {
   const [showNav, setShowNav] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    // create a media query listener for screen widths at least 768px
+    const mediaWatcher = window.matchMedia('(min-width: 768px)');
+    // update the state based on the media query match
+    const updateIsWideScreen = (e) => {
+      setIsWideScreen(e.matches);
+      setShowNav(false);
+    };
+
+    mediaWatcher.addEventListener('change', updateIsWideScreen);
+    // set the initial state based on the current screen width
+    updateIsWideScreen(mediaWatcher);
+
+    return () => {
+      mediaWatcher.removeEventListener('change', updateIsWideScreen);
+    };
+  }, []);
 
   const toggleNav = (isShown) => {
     setShowNav(isShown);
+  };
+
+  const closeNav = () => {
+    setShowNav(false);
   };
 
   return (
@@ -16,7 +39,7 @@ function Header() {
         <p className="header-logo">huseva</p>
         <NavToggler onToggle={toggleNav} isShown={showNav} />
       </div>
-      <Nav isShown={showNav} />
+      <Nav isShown={showNav} onClose={closeNav} />
     </header>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function Nav({ isShown }) {
-  const [isWideScreen, setIsWideScreen] = useState(false);
+function Nav({ isShown, onClose }) {
+  const navClassShow = isShown ? 'nav-show' : '';
 
-  useEffect(() => {
-    // create a media query listener for screen widths at least 768px
-    const mediaWatcher = window.matchMedia('(min-width: 768px)');
-    // update the state based on the media query match
-    const updateIsWideScreen = (e) => {
-      setIsWideScreen(e.matches);
-    };
-
-    mediaWatcher.addEventListener('change', updateIsWideScreen);
-    // set the initial state based on the current screen width
-    updateIsWideScreen(mediaWatcher);
-
-    return () => {
-      mediaWatcher.removeEventListener('change', updateIsWideScreen);
-    };
-  }, []);
-
-  const navClassShow = isShown && !isWideScreen ? 'nav-show' : '';
+  // smooth scrolling to the correct section with improved offset height
+  const handleClick = (e, targetId) => {
+    e.preventDefault();
+    const navHeight = 32;
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - navHeight,
+        behavior: 'smooth',
+      });
+    }
+    // remove focus from the element
+    e.target.blur();
+    // close nav
+    onClose();
+  };
 
   return (
     <nav className={`nav ${navClassShow}`}>
       <ul className="nav-list">
         <li className="nav-list-item">
-          <a href="#about">about me</a>
+          <a href="#" onClick={(e) => handleClick(e, 'about')}>
+            about me
+          </a>
         </li>
         <li className="nav-list-item">
-          <a href="#projects">my projects</a>
+          <a href="#" onClick={(e) => handleClick(e, 'projects')}>
+            my projects
+          </a>
         </li>
         <li className="nav-list-item">
           <a className="nav-link-contact" href="mailto:huseva.a@gmail.com">
